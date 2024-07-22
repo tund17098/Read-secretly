8bf977d268473f24100e7c8961c2f688179a0859
@tu.nguyen.duc <tu.nguyen.duc@247express.vn>
2024-07-22 15:59:47 +0700

    update

---
+++ components/index.js
@@ -0,0 +1,8 @@
+document.getElementById('search-button').addEventListener('click', function() {
+      const searchTerm = document.getElementById('search-input').value;
+
+      
+  });
+  
+  // Bạn có thể thêm các chức năng JavaScript khác tại đây
+  
